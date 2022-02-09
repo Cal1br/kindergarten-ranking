7aa299bb55f324629c18d97fdddaf5c0536cafb8
Cal1br <atillamexmed3@gmail.com>
2022-02-10 01:41:17 +0200

    chore: changes in steps, implementing more steps

--- src/test/java/me/cal1br/kindergartenranking/child/wishlistSteps.java
+++ src/test/java/me/cal1br/kindergartenranking/child/wishlistSteps.java
@@ -1,12 +1,16 @@
 package me.cal1br.kindergartenranking.child;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import me.cal1br.kindergartenranking.child.model.ChildModel;
 import me.cal1br.kindergartenranking.child.repository.ChildRepository;
 import me.cal1br.kindergartenranking.child.repository.ChildRepositoryImpl;
+import me.cal1br.kindergartenranking.child.service.ChildService;
+import me.cal1br.kindergartenranking.child.service.ChildServiceImpl;
 import me.cal1br.kindergartenranking.kindergarten.model.KindergartenModel;
 import me.cal1br.kindergartenranking.kindergarten.repository.KindergartenRepository;
 import me.cal1br.kindergartenranking.kindergarten.repository.KindergartenRepositoryImpl;
@@ -15,8 +19,11 @@ import me.cal1br.kindergartenranking.kindergarten.service.KindergartenServiceImp
 import me.cal1br.kindergartenranking.parent.repository.ParentRepository;
 import me.cal1br.kindergartenranking.parent.repository.ParentRepositoryImpl;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Collections;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -27,41 +34,69 @@ import static org.junit.Assert.assertTrue;
 
 public class wishlistSteps {
 
-    private final ChildModel child = new ChildModel();
-    private final List<KindergartenModel> wishList = new LinkedList<>();
-    private KindergartenService kindergartenService;
-    private Map<KindergartenModel, List<ChildModel>> rankedChildrenByKG;
+    private final ChildModel successfulChild = new ChildModel();
+    private final ChildModel noWishlistChild = new ChildModel();
+    //Repositories
+    private final KindergartenRepository kindergartenRepository = new KindergartenRepositoryImpl();
+    private final ChildRepository childRepository = new ChildRepositoryImpl();
+    private final ParentRepository parentRepository = new ParentRepositoryImpl();
+    //Services
+    private final KindergartenService kindergartenService = new KindergartenServiceImpl(parentRepository, childRepository, kindergartenRepository);
+    private final ChildService childService = new ChildServiceImpl(childRepository);
+    //private final ChildModel secondWishChild = new ChildModel();
+    private Map<KindergartenModel, List<ChildModel>> rankedChildrenByKG = new HashMap<>();
 
-    @Before
-    public void setUp() {
+    {
         final KindergartenModel kindergarten0 = new KindergartenModel();
         kindergarten0.setName("Zdravec");
         kindergarten0.setPlaces(5);
         final KindergartenModel kindergarten1 = new KindergartenModel();
         kindergarten1.setName("KinderSurprise");
         kindergarten1.setPlaces(5);
-        final KindergartenRepository kindergartenRepository = new KindergartenRepositoryImpl();
-        kindergartenRepository.save(kindergarten0);
-        kindergartenRepository.save(kindergarten1);
-        wishList.addAll(kindergartenRepository.getAll());
-        final ChildRepository childRepository = new ChildRepositoryImpl();
-        kindergartenRepository.save(kindergarten0);
-        kindergartenRepository.save(kindergarten1);
-        final ParentRepository parentRepository = new ParentRepositoryImpl();
-        kindergartenService = new KindergartenServiceImpl(parentRepository, childRepository, kindergartenRepository);
+        kindergartenService.save(kindergarten0);
+        kindergartenService.save(kindergarten1);
+
+        successfulChild.setName("Artyom");
+        successfulChild.setDateOfBirth(LocalDateTime.ofInstant(Instant.now(), ZoneId.of("UTC")).minusYears(5));
+        noWishlistChild.setName("Bernard");
+        noWishlistChild.setDateOfBirth(LocalDateTime.ofInstant(Instant.now(), ZoneId.of("UTC")).minusYears(4));
+        //secondWishChild.setName("Charlie");
+    }
+
+    @Before
+    public void setUp() {
+        successfulChild.setWishList(kindergartenService.findAll());
+        childService.save(successfulChild);
+        childService.save(noWishlistChild);
+    }
+
+    @Given("^There is a child$")
+    public void thereIsAChild() {
+        assertFalse(childService.findAll().isEmpty());
+    }
+
+    @And("^There is a kindergarten$")
+    public void thereIsAKindergarten() {
+        assertFalse(kindergartenService.findAll().isEmpty());
+    }
+
+    @Given("^Child has no wishlist$")
+    public void childHasNoWishlist() throws Throwable {
+        noWishlistChild.setWishList(Collections.emptyList());
+        childService.editById(noWishlistChild.getId(), noWishlistChild);
+        assertTrue(childService.findById(noWishlistChild.getId()).getWishList().isEmpty());
     }
 
     @Given("^Child has wishlist$")
     public void childHasWishlist() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        child.setWishList(wishList);
-        assertNotNull(child.getWishList());
-        assertFalse(child.getWishList().isEmpty());
+        assertNotNull(successfulChild.getWishList());
+        assertFalse(successfulChild.getWishList().isEmpty());
     }
 
     @When("^Ranking is processed$")
     public void rankingIsProcessed() throws Throwable {
-        rankedChildrenByKG = kindergartenService.rankChildren(kindergartenService.findAll(), Collections.singletonList(child));
+        rankedChildrenByKG = kindergartenService.rankChildren(kindergartenService.findAll(), Collections.singletonList(successfulChild));
         assertNotNull(rankedChildrenByKG.keySet());
         assertNotNull(rankedChildrenByKG.values());
     }
@@ -72,6 +107,28 @@ public class wishlistSteps {
                 .stream()
                 .flatMap(List::stream)
                 .collect(Collectors.toList())
-                .contains(child));
+                .contains(successfulChild));
+    }
+
+    @Then("^Child isn't accepted anywhere$")
+    public void childIsnTAcceptedAnywhere() throws Throwable {
+        assertFalse(rankedChildrenByKG.values()
+                .stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList())
+                .contains(noWishlistChild));
+    }
+
+    @Then("^Child is accepted into second kindergarten$")
+    public void childIsAcceptedIntoSecondKindergarten() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @Given("^Kindergarten has no spots$")
+    public void kindergartenHasNoSpots() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
